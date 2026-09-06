f5ba0feb8d8c027ff890851bce53cb60c3b8932b
tomek <>
2026-09-06 15:12:38 +0200

    Switched

--- src/main/kotlin/net/knaur/Main.kt
+++ src/main/kotlin/net/knaur/Main.kt
@@ -1,11 +1,14 @@
 package net.knaur
 
+import net.knaur.model.ResponseMessage
 import org.http4k.core.*
 import org.http4k.core.Method.GET
 import org.http4k.routing.bind
 import org.http4k.routing.routes
 import org.http4k.server.Jetty
 import org.http4k.server.asServer
+import org.http4k.format.Jackson.auto
+import java.time.Instant
 
 fun getGreeting(): String {
     val osName = System.getProperty("os.name")
@@ -15,12 +18,19 @@ fun getGreeting(): String {
 
 fun main() {
     println(getGreeting())
-    print("Webapp is running on port 7070")
+    println("Webapp is running on port 7070")
 
-    // Simple route registry: add new entries here to expose more routes.
+    val messageLens = Body.auto<ResponseMessage>().toLens()
+
     val app: HttpHandler = routes(
-        "/" bind GET to { _: Request -> Response(Status.OK).body("Hello World") },
-        "/greeting" bind GET to { _: Request -> Response(Status.OK).body(getGreeting()) }
+        "/" bind GET to { _: Request ->
+            val msg = ResponseMessage(Instant.now().epochSecond, "Hello World")
+            Response(Status.OK).with(messageLens of msg)
+        },
+        "/greeting" bind GET to { _: Request ->
+            val msg = ResponseMessage(Instant.now().epochSecond, getGreeting())
+            Response(Status.OK).with(messageLens of msg)
+        }
     )
 
     // Start the server on port 7070 using Jetty adapter
---
+++ src/main/kotlin/net/knaur/model/ResponseMessage.kt
@@ -0,0 +1,3 @@
+package net.knaur.model
+
+data class ResponseMessage(val timestamp: Long, val content: String)
